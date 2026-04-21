c48377e2de59e74e7cd09f5c5348b52e3a804744
Joseph Perez <joperez@hotmail.fr>
2026-04-21 22:29:09 +0200

    Put WaitQueue behind a feature

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,10 @@ pub mod queue;
 pub mod sync;
 #[cfg(not(nightly))]
 mod unsafe_pinned;
+#[cfg(feature = "wait-queue")]
 pub mod wait_queue;
 
 pub use node::{Node, NodeState};
 pub use queue::Queue;
+#[cfg(feature = "wait-queue")]
 pub use wait_queue::WaitQueue;
